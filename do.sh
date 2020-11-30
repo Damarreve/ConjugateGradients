6bd272b1b8db221307465357965a5db88fc91fd7
Danil Filippov <damarreve@list.ru>
2020-12-01 00:18:56 +0400

    Наработки

---
+++ do.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+julia --version >/dev/null 2>&1
+
+[[ $? -ne 0 ]] && echo -e "Приложение \033[0;34mJulia\033[0m \033[1mне найдено\033[0m!" && exit 127
+
+pushd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1
+for File in $(find ./src -type f -name *.jl)
+do
+    echo -e "\033[0;4$[${RANDOM}%5+1]m"$(basename ${File})"\033[0m:"
+    julia ${File}
+    echo
+done
+popd >/dev/null 2>&1
--- src/ConjugateGradientsMethod.jl
+++ src/ConjugateGradientsMethod.jl
@@ -1,5 +1,58 @@
-matrix_A = [3 4 0; 4 -3 0; 0 0 5]
-vector_b = [1 5 9]
+using SparseArrays
+using LinearAlgebra
 
-println("A: ", matrix_A)
-println("b: ", vector_b)
+# Чтение разреженного вектора из файла
+function read_csc_vector(filename)
+  file = open(filename, "r")
+  colptr = parse.(Int, split(readline(file)))
+  nzval = parse.(Float64, split(readline(file)))
+  close(file)
+  return sparsevec(colptr, nzval)
+end
+
+# Чтение разреженной матрицы из файла
+function read_csc_matrix(filename)
+  file = open(filename, "r")
+  colptr = parse.(Int, split(readline(file)))
+  rowval = parse.(Int, split(readline(file)))
+  nzval = parse.(Float64, split(readline(file)))
+  close(file)
+  return sparse(colptr, rowval, nzval)
+end
+
+matrix_A = read_csc_matrix("resources/little_matrix.csc")
+vector_b = read_csc_vector("resources/little_vector.csc")
+
+vector_x = Array{Float64,1}(zeros(length(vector_b))) .+ 0.2
+vector_r = vector_b - (matrix_A * vector_x)
+vector_z = vector_r
+
+println("vector_b: ", vector_b)
+println("matrix_A: ", matrix_A)
+println("vector_x0: ", vector_x)
+println("vector_r0: ", vector_r)
+println("vector_z0: ", vector_z)
+
+eps = 0.1
+println(norm(vector_r) / norm(vector_b))
+println(eps)
+i = 1
+# TODO: Очень кривой алгоритм, не работает!
+while eps < (norm(vector_r) / norm(vector_b))
+  local vector_xp = vector_x
+  local vector_rp = vector_r
+  local vector_zp = vector_z
+  alpha = (transpose(vector_rp) * vector_rp) / (transpose(matrix_A * vector_zp) * vector_zp)
+  println("alpha[", i, "]: ", alpha)
+  global vector_x = vector_xp + alpha * vector_zp
+  println("vector_x[", i, "]: ", vector_x)
+  global vector_r = vector_rp - alpha * (matrix_A * vector_zp)
+  println("vector_r[", i, "]: ", vector_r)
+  println("vector_rm[", i, "]: ", vector_rp)
+  beta = (transpose(vector_r) * vector_r) / (transpose(vector_rp) * vector_rp)
+  println("beta[", i, "]: ", beta)
+  global vector_z = vector_r + beta * vector_zp
+  println("vector_z[", i, "]: ", vector_z)
+  println()
+  global i += 1
+end
